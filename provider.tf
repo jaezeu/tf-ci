da6e24eed14a295f1b19ab5346340f82ccdfc8a5
Jazeel Meerasah <jazeel_18@hotmail.com>
2024-02-07 11:57:25 +0800

    tf fixes

--- provider.tf
+++ provider.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 1.6"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.35.0"
+      version = "~> 5.0"
     }
   }
 }
